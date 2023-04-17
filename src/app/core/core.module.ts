ac5016bd6ec27b62b3fe23812129c05da2209215
Vladimir <v.milashevsky@gmail.com>
2023-04-17 12:39:15 +0300

    feat: add logo component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { LogoComponent } from './components/header/components/logo/logo.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, LogoComponent],
   exports: [HeaderComponent, FooterComponent],
   imports: [BrowserModule, CommonModule, SharedModule],
 })
