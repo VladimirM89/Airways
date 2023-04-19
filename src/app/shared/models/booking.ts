6a59a3a4a10f0eff4a6b8c2d1302b09a8897677f
Ponomareva Nina <nina.9412@mail.ru>
2023-04-19 15:48:44 +0300

    feat: add basic models and intefaces for booking

---
+++ src/app/shared/models/booking.ts
@@ -0,0 +1,34 @@
+export interface BookingInfo {
+  roundTrip: boolean;
+  departureCity: string;
+  destinationCity: string;
+  departureDate: string;
+  returnDate: string;
+  passengers: PassengersNumber;
+}
+
+export interface PassengersNumber {
+  adults: number;
+  child: number;
+  infants: number;
+}
+
+export interface PassangersInfo {
+  adults: Array<Passenger>;
+  child: Array<Passenger>;
+  infants: Array<Passenger>;
+  contacts: ContactInfo;
+}
+
+export interface ContactInfo {
+  email: string;
+  mobile: string;
+}
+
+export interface Passenger {
+  firstName: string;
+  lastName: string;
+  sex: string;
+  dateOfBirth: string;
+  specialAssistance?: boolean;
+}
---
+++ src/app/shared/models/flight-item.ts
@@ -0,0 +1,13 @@
+export interface FlightItem {
+  id: string;
+  flightNumber: string;
+  departureCity: string;
+  destinationCity: string;
+  departureDate: string;
+  destinationDate: string;
+  departureTime: string;
+  destinationTime: string;
+  flightDuration: string;
+  price: number;
+  seats: number;
+}
