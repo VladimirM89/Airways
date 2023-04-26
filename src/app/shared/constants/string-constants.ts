7627fa2a520ae173bc1950ac28d7f4861564d36d
Vladimir <v.milashevsky@gmail.com>
2023-04-26 10:32:47 +0300

    fix: fix at least check one letter and one number in regexp

--- src/app/shared/constants/string-constants.ts
+++ src/app/shared/constants/string-constants.ts
@@ -1,2 +1,6 @@
 export const NAME_REGEXP = /^[a-zA-Z,-]+$/;
 export const PHONE_REGEXP = /^\+?[0-9]\d{1,20}$/;
+export const CASE_CHECK_REGEXP = /(?=.*[a-z])(?=.*[A-Z])/;
+export const LETTERS_NUMBERS_CHECK_REGEXP =
+  /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)|(?=.*\W)$/;
+export const SPECIAL_SYMBOLS_CHECK_REGEXP = /(?=.*\W)/;
--- src/app/shared/validators/password.validators.ts
+++ src/app/shared/validators/password.validators.ts
@@ -1,4 +1,9 @@
 import { FormControl } from '@angular/forms';
+import {
+  CASE_CHECK_REGEXP,
+  LETTERS_NUMBERS_CHECK_REGEXP,
+  SPECIAL_SYMBOLS_CHECK_REGEXP,
+} from '../constants/string-constants';
 
 export default class PasswordValidators {
   public static checkStrongPassword(
@@ -10,15 +15,15 @@ export default class PasswordValidators {
       errors.push('at least 8 characters');
     }
 
-    if (!control.value.match(/(?=.*[a-z])(?=.*[A-Z])/)) {
+    if (!control.value.match(CASE_CHECK_REGEXP)) {
       errors.push('a mixture of both uppercase and lowercase letters');
     }
 
-    if (!control.value.match(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)) {
+    if (!control.value.match(LETTERS_NUMBERS_CHECK_REGEXP)) {
       errors.push('a mixture of letters and numbers');
     }
 
-    if (!control.value.match(/(?=.*\W)/)) {
+    if (!control.value.match(SPECIAL_SYMBOLS_CHECK_REGEXP)) {
       errors.push(
         'inclusion of at least one special character, e.g., ! @ # ? ]'
       );
