ebc5ea388d5eb8c388c35692249a3bc4eb076c76
Vladimir <v.milashevsky@gmail.com>
2023-04-26 10:38:36 +0300

    refactor: add string constants in password validator

--- src/app/shared/validators/password.validators.ts
+++ src/app/shared/validators/password.validators.ts
@@ -2,7 +2,11 @@ import { FormControl } from '@angular/forms';
 import {
   CASE_CHECK_REGEXP,
   LETTERS_NUMBERS_CHECK_REGEXP,
+  MINIMUM_LENGHT_TEXT,
+  MIX_CASES_TEXT,
+  MIX_LETTERS_NUMBERS_TEXT,
   SPECIAL_SYMBOLS_CHECK_REGEXP,
+  SPECIAL_SYMBOLS_TEXT,
 } from '../constants/string-constants';
 
 export default class PasswordValidators {
@@ -12,21 +16,19 @@ export default class PasswordValidators {
     const errors = [];
 
     if (!(control.value.length >= 8)) {
-      errors.push('at least 8 characters');
+      errors.push(MINIMUM_LENGHT_TEXT);
     }
 
     if (!control.value.match(CASE_CHECK_REGEXP)) {
-      errors.push('a mixture of both uppercase and lowercase letters');
+      errors.push(MIX_CASES_TEXT);
     }
 
     if (!control.value.match(LETTERS_NUMBERS_CHECK_REGEXP)) {
-      errors.push('a mixture of letters and numbers');
+      errors.push(MIX_LETTERS_NUMBERS_TEXT);
     }
 
     if (!control.value.match(SPECIAL_SYMBOLS_CHECK_REGEXP)) {
-      errors.push(
-        'inclusion of at least one special character, e.g., ! @ # ? ]'
-      );
+      errors.push(SPECIAL_SYMBOLS_TEXT);
     }
     return errors.length ? { checkStrongPassword: errors } : null;
   }
