5e6a641445598a445d671b04ede9d0ddd8fb142e
Ponomareva Nina <nina.9412@mail.ru>
2023-05-10 16:59:18 +0300

    feat: update api service - add methods for flights

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BASE_URL } from 'src/app/shared/constants/api-constants';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
+import { FlightItem } from 'src/app/shared/models/flight-item';
 
 @Injectable({
   providedIn: 'root',
@@ -9,27 +10,28 @@ import { Observable, tap } from 'rxjs';
 export class ApiService {
   public constructor(private http: HttpClient) {}
 
-  public getAllFlights(): Observable<any> {
-    return this.http.get(`${BASE_URL}/flights/all`, {}).pipe(
-      tap(response => {
-        console.log(response);
-      })
-    );
+  public getAllFlights(): Observable<FlightItem[]> {
+    return this.http.get<FlightItem[]>(`${BASE_URL}/flights/all`, {});
   }
 
-  public getFlights(): Observable<any> {
-    return this.http
-      .get(`${BASE_URL}/flights`, {
-        params: {
-          departureAirport: 'ABZ',
-          destinationAirport: 'GYD',
-          departureDate: '2023-05-05',
-        },
-      })
-      .pipe(
-        tap(response => {
-          console.log(response);
-        })
-      );
+  public getFlights(
+    departure: string,
+    destination: string,
+    date: string
+  ): Observable<FlightItem[]> {
+    return this.http.get<FlightItem[]>(`${BASE_URL}/flights`, {
+      params: {
+        departureAirport: departure,
+        destinationAirport: destination,
+        departureDate: date,
+      },
+    });
+  }
+
+  public generateFlights(from: string, to: string): Observable<FlightItem[]> {
+    return this.http.post<FlightItem[]>(`${BASE_URL}/flights`, {
+      fromDate: from,
+      toDate: to,
+    });
   }
 }
