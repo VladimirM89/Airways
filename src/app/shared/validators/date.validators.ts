d1f1f05b9b3565f87b6a5c860442fba775935c7f
Ponomareva Nina <nina.9412@mail.ru>
2023-05-20 03:52:57 +0300

    create validator functions for date pickers

--- src/app/shared/validators/date.validators.ts
+++ src/app/shared/validators/date.validators.ts
@@ -1,6 +1,11 @@
-import { FormControl } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
 
-export default class DateValidators {
+export class DateValidators {
   public static isFutureDate(
     control: FormControl<Date>
   ): { [key: string]: boolean } | null {
@@ -11,4 +16,45 @@ export default class DateValidators {
     }
     return { isFutureDate: true };
   }
+}
+
+export function isDateInPast(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (control.value) {
+      const controlDate = control.value.getTime();
+      const currentDate = new Date().getTime();
+      if (currentDate - controlDate > 0) {
+        return { isDateInPast: true };
+      }
+    }
+    return null;
+  };
+}
+
+export function isFlightsDateRangeValid(): ValidatorFn {
+  return (rangeFormGroup: AbstractControl): ValidationErrors | null => {
+    const departureDate = rangeFormGroup.get('departureDate')?.value;
+    const destinationDate = rangeFormGroup.get('destinationDate')?.value;
+    const currentDate = new Date().getTime();
+
+    if (!departureDate && !destinationDate) {
+      return null;
+    }
+
+    if (!destinationDate) {
+      return { isRangeValid: 'please select destination date' };
+    }
+
+    if (departureDate.getTime() - destinationDate.getTime() > 0) {
+      return { isRangeValid: 'destination date can not be before departure' };
+    }
+
+    if (
+      currentDate - departureDate.getTime() > 0 ||
+      currentDate - destinationDate.getTime() > 0
+    ) {
+      return { isRangeValid: 'please select dates in future' };
+    }
+    return null;
+  };
 }
