4b71d9c8cbaed098e4a358324492f980b415d54b
Vladimir <v.milashevsky@gmail.com>
2023-05-24 02:15:21 +0300

    feat: add layout booking table component

--- src/app/shared/components/booking-table/booking-table.component.ts
+++ src/app/shared/components/booking-table/booking-table.component.ts
@@ -1,5 +1,9 @@
+/* eslint-disable no-return-assign */
 import { Component } from '@angular/core';
 import { BookingService } from 'src/app/core/services/booking.service';
+import { Paths } from 'src/app/types/enums';
+import { Router } from '@angular/router';
+import { PaymentService } from 'src/app/core/services/payment.service';
 import { UserBooking } from '../../models/user.model';
 
 @Component({
@@ -8,10 +12,28 @@ import { UserBooking } from '../../models/user.model';
   styleUrls: ['./booking-table.component.scss'],
 })
 export class BookingTableComponent {
-  public constructor(private bookingService: BookingService) {}
+  public constructor(
+    private bookingService: BookingService,
+    private router: Router,
+    private paymentService: PaymentService
+  ) {}
 
   public get userBookings(): UserBooking[] {
     console.log('booking table: ', this.bookingService.userBookings);
     return this.bookingService.userBookings;
+  }
+
+  public navToMain(): void {
+    this.router.navigate([Paths.BOOKING]);
+  }
+
+  public get summary(): number {
+    let sum = 0;
+    const bookings = this.bookingService.userBookings;
+    bookings.forEach(
+      item =>
+        (sum += this.paymentService.summary(item.bookingInfo, item.flights))
+    );
+    return sum;
   }
 }
