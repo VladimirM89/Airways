10b7839a440d2e9445c905dca7c8d6f2f60b27c5
Vladimir <v.milashevsky@gmail.com>
2023-05-26 20:21:42 +0300

    feat: test redirect to server

--- src/app/redux/reducers/user.reducer.ts
+++ src/app/redux/reducers/user.reducer.ts
@@ -12,17 +12,7 @@ export interface UserState {
 }
 
 const initialState: UserState = {
-  userDate: {
-    id: 2,
-    email: 'vms@mail.com',
-    password: 'Aa!11111',
-    firstName: 'Vladimir',
-    lastName: 'Milashevsky',
-    dateOfBirth: '08.06.1992',
-    sex: 'male',
-    pnone: '+375297739618',
-    citizenship: 'Belarus',
-  },
+  userDate: null,
   bookings: [
     {
       id: 1,
--- src/app/shared/constants/api-constants.ts
+++ src/app/shared/constants/api-constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://45.147.179.67:7000';
+export const BASE_URL = '/api';
