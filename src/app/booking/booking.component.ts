fc45c302b886a9704439b3603e41b683ab980c20
Ponomareva Nina <nina.9412@mail.ru>
2023-06-05 19:41:19 +0300

    feat: handle booking page reload

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,12 +1,22 @@
-import { Component, HostListener, OnDestroy } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { LocalStorageService } from '../core/services/local-storage.service';
+import { BookingService } from '../core/services/booking.service';
 
 @Component({
   selector: 'app-booking-page',
   templateUrl: './booking.component.html',
 })
-export class BookingComponent implements OnDestroy {
-  public constructor(private localStorageService: LocalStorageService) {}
+export class BookingComponent implements OnDestroy, OnInit {
+  public constructor(
+    private localStorageService: LocalStorageService,
+    private bookingService: BookingService
+  ) {}
+
+  public ngOnInit(): void {
+    if (!this.bookingService.getCurrentBookingInfo()) {
+      this.localStorageService.getBookingFromLocalStorage();
+    }
+  }
 
   @HostListener('window:beforeunload', ['$event'])
   public beforeUnloadHandler(): void {
