285dcf1fa1fe4311ffc0d3a969b560f7fe645281
Ponomareva Nina <nina.9412@mail.ru>
2023-07-06 15:55:21 +0300

    feat: update adaptive design of header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -4,6 +4,8 @@ import { Subscription } from 'rxjs';
 
 import { RouterService } from 'src/app/core/services/router.service';
 import { Paths } from 'src/app/types/enums';
+import { selectUserAuthBoolean } from 'src/app/redux/selectors/user.selectors';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-header',
@@ -11,11 +13,16 @@ import { Paths } from 'src/app/types/enums';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+  public constructor(
+    private routerService: RouterService,
+    private store: Store
+  ) {}
+
   public isMainPage = true;
 
   private subStyle!: Subscription;
 
-  public constructor(private routerService: RouterService) {}
+  public isUserLoggedIn = this.store.select(selectUserAuthBoolean);
 
   public ngOnInit(): void {
     this.subStyle = this.routerService.checkUrl().subscribe(event => {
